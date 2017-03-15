2bb045cf4fcef55d27945a78705f339717d6ee42
jacksdy <asdrubalgg@gmail.com>
2017-03-15 09:56:37 -0400

    Esto es una Prueba para probar la utilización del Repo

---
+++ ejemploJS.js
@@ -0,0 +1,16 @@
+    <!--//-->
+    < script language = "JavaScript" >
+        <!-- 
+        var id, pause = 0,
+            position = 0;
+
+    function scorrevole() {
+        var i, k, msg = "               Esto es para la publicidad                   ";
+        k = (100 / msg.length) + 1;
+        for (i = 0; i <= k; i++) msg += " " + msg;
+        document.form2.scorrevole.value = msg.substring(position, position + 100);
+        if (position++ == 100) position = 0;
+        id = setTimeout("scorrevole()", 100)
+    }
+    //-->
+    < /script>
---
+++ holaMundo.js
@@ -0,0 +1,3 @@
+let nombre = "Asdurbal "
+let apellido = "González"
+console.log("Hola como estas " + nombre + apellido);
